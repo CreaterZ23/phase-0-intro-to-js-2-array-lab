299a3aec26ac30c33fe0823f1140a559f91e6895
Zeon <zeonstewart23@gmail.com>
2021-08-11 11:26:24 -0400

    CompletedAssignment

--- index.js
+++ index.js
@@ -1,39 +1,75 @@
-// Write your solution here!
+// // Write your solution here!
+// const cats = ["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendCat(name) {
+//     cats.push(name);
+//     return;
+// } 
+
+// function destructivelyPrependCat(name) {
+//    cats.unshift(name);
+// }
+
+// function destructivelyRemoveLastCat(name) {
+//     cats.pop(name);
+// }
+
+// function destructivelyRemoveFirstCat(name) {
+//     cats.shift(name);
+// }
+
+// function appendCat(name) {
+//     const newCats = [...cats, name];
+//     return newCats;
+// }
+
+// function prependCat(name) {
+//     const newCats = [name, ...cats];
+//     return newCats;
+// }
+
+// function removeLastCat() {
+//     const newCats = cats.slice(0,2);
+//     return newCats;
+// }
+
+// function removeFirstCat() {
+//     const newCats = cats.slice(1);
+//     return newCats;
+// }
+
 const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
-    cats.push(name);
-    return;
-} 
+    return cats.push(name);
+}
 
 function destructivelyPrependCat(name) {
-   cats.unshift(name);
+    return cats.unshift(name);
 }
 
-function destructivelyRemoveLastCat(name) {
-    cats.pop(name);
+function destructivelyRemoveFirstCat() {
+    return cats.splice(0, 1);
 }
 
-function destructivelyRemoveFirstCat(name) {
-    cats.shift(name);
+function destructivelyRemoveLastCat() {
+    return cats.splice(2, 1);
 }
 
 function appendCat(name) {
-    const newCats = [...cats, name];
+    let newCats = [...cats, name];
     return newCats;
 }
 
 function prependCat(name) {
-    const newCats = [name, ...cats];
+    let newCats = [name, ...cats];
     return newCats;
 }
 
 function removeLastCat() {
-    const newCats = cats.slice(0,2);
-    return newCats;
+    return cats.slice(0, 2);
 }
 
 function removeFirstCat() {
-    const newCats = cats.slice(1);
-    return newCats;
+    return cats.slice(1);
 }
